03789312ae9c7455d0c566b1e8925d171cc800d2
Esteban Principe <40151035+Miunmn@users.noreply.github.com>
2020-11-24 20:54:47 -0500

    Create CmCirculos.h

---
+++ Proyecto/CmCirculos.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef PACMAN_CMCIRCULOS_H
+	#define PACMAN_CMCIRCULOS_H
+
+	#include "CCirculo.h"
+
+class CmCirculos
+{
+private:
+	sf::RenderWindow* palCanvas = nullptr;
+	vector<CCirculo> v;
+
+public:
+	CmCirculos(sf::RenderWindow* _palCanvas, size_t n);
+	~CmCirculos();
+	void mostrarCirculos();
+	void moverCirculos();
+
+	vector<CCirculo> getVectordeCirculos()
+	{
+		return v;
+	}
+};
+
+#endif //PACMAN_CMCIRCULOS_H
