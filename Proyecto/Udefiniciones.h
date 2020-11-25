8c5152d1b2dc267f5424a4a05884364118d12211
Esteban Principe <40151035+Miunmn@users.noreply.github.com>
2020-11-24 20:55:14 -0500

    Create Udefiniciones.h

---
+++ Proyecto/Udefiniciones.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef PACMAN_UDEFINICIONES_H
+	#define PACMAN_UDEFINICIONES_H
+
+	#include <SFML/Graphics.hpp>
+	#include <iostream>
+	#include <vector>
+using namespace std;
+
+typedef float tnumeroReal;
+typedef unsigned int tcolor;
+typedef unsigned int tentero;
+
+#endif //PACMAN_UDEFINICIONES_H
